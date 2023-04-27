d7a4eb577ac5af50bd4cd20e30cc74814c80860f
Guilherme Minozzi <minozzi60@gmail.com>
2023-04-26 21:06:54 -0300

    feat(services/api): add ListParams type

--- src/services/api/types.ts
+++ src/services/api/types.ts
@@ -1,15 +1,17 @@
 import { HttpStatusCode } from 'axios'
 
+type Filter = Record<string, string>
+
 export interface HttpClient<
   TData = unknown,
-  TFilters = Record<string, string>
+  TFilters extends Filter[] = Filter[]
 > {
   request: (data: HttpRequest<TFilters>) => Promise<HttpResponse<TData>>
 }
 
 export type HttpMethod = 'get' | 'post' | 'delete' | 'patch'
 
-export type HttpRequest<TFilters = Record<string, string>> = {
+export type HttpRequest<TFilters extends Filter[] = Filter[]> = {
   url: string
   method: HttpMethod
   body?: unknown
@@ -24,3 +26,5 @@ export type HttpResponse<TData = unknown> = {
   statusCode: HttpStatusCode
   body?: TData
 }
+
+export type ListParams = Pick<HttpRequest, 'filters' | 'pagination'>
