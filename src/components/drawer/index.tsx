ff78fbadbec54b05f8db21a22ff246ef10c6b713
Guilherme Minozzi <minozzi60@gmail.com>
2023-05-02 21:52:22 -0300

    feat(components/drawer): add title prop

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -3,8 +3,13 @@ import React from 'react'
 import * as S from './styles'
 import { DrawerProps } from './types'
 
-export const Drawer: React.FC<DrawerProps> = ({ open, children, onClose }) => (
-  <S.Drawer open={open} onClose={onClose} anchor="right">
+export const Drawer: React.FC<DrawerProps> = ({
+  open,
+  title,
+  children,
+  onClose
+}) => (
+  <S.Drawer open={open} onClose={onClose} anchor="right" title={title}>
     <S.CloseIcon onClick={onClose} />
     {children}
   </S.Drawer>
--- src/components/drawer/types.ts
+++ src/components/drawer/types.ts
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react'
 
 type Props = {
+  title: string
   open: boolean
   onClose: () => void
 }
