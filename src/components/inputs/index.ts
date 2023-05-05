b6cc4db0a547f682483940e61e04cc498624b4a9
Guilherme Minozzi <minozzi60@gmail.com>
2023-05-05 20:57:35 -0300

    chore(components): export autocomplete and multselect

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,2 +1,4 @@
+export * from '@/components/inputs/autocomplete'
+export * from '@/components/inputs/multSelect'
 export * from '@/components/inputs/password'
 export * from '@/components/inputs/textField'
