008860cb5d934400a737519be9715bca87e9d9aa
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2023-05-09 03:36:51 -0300

    validarion

--- src/components/form-drx/index.tsx
+++ src/components/form-drx/index.tsx
@@ -25,7 +25,15 @@ const rows = [
 ];
 
 const validationForm = yup.object().shape({
-
+  nomeAluno: yup.string().required("Informe seu nome"),
+  emailAluno: yup.string().email("Email inválido").required("Informe seu email"),
+  telefoneAluno: yup.string().required("Informe seu telefone"),
+  nomeOrientador: yup.string().required("Informe o nome do seu orientador"),
+  emailOrientador: yup.string().email("Email inválido").required("Informe o email do seu orientador"),
+  telefoneOrientador: yup.string().required("Informe o telefone"),
+  departamento: yup.string().required("Informe o departamento"),
+  naturezaProjeto: yup.string().required("Informe a natureza do projeto"),
+  descricao: yup.string().required("Informe a descrição")
 });
 
 // function addInTable(values: {
@@ -45,7 +53,15 @@ const validationForm = yup.object().shape({
 // }
 
 async function handleClickForm(values: {
-
+  nomeAluno: string;
+  emailAluno: string;
+  telefoneAluno: string;
+  nomeOrientador: string;
+  emailOrientador: string;
+  telefoneOrientador: string;
+  departamento: string;
+  naturezaProjeto: string;
+  descricao: string;
 }) {
   try {
 
@@ -60,6 +76,15 @@ export const FormDrx: React.FC = () => (
     <div>
       <Formik
         initialValues={{
+          nomeAluno: "",
+          emailAluno: "",
+          telefoneAluno: "",
+          nomeOrientador: "",
+          emailOrientador: "",
+          telefoneOrientador: "",
+          departamento: "",
+          naturezaProjeto: "",
+          descricao: "",
         }}
         onSubmit={handleClickForm}
         validationSchema={validationForm}
@@ -207,7 +232,8 @@ export const FormDrx: React.FC = () => (
                 </TableBody>
               </Table>
             </TableContainer>
-          </div>  
+          </div>
+          <FormFooter /> 
         </Form>
       </Formik>
     </div>
