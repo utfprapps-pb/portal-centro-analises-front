f0e43c7d8a2a3e295642757502de04ee3036e4db
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2023-05-11 20:21:55 -0300

    tabela

--- src/components/form-drx/index.tsx
+++ src/components/form-drx/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from "yup";
 import styles from "./styles.module.scss";
@@ -12,230 +12,249 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Add } from '@material-ui/icons'
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-const validationForm = yup.object().shape({
-  nomeAluno: yup.string().required("Informe seu nome"),
-  emailAluno: yup.string().email("Email inválido").required("Informe seu email"),
-  telefoneAluno: yup.string().required("Informe seu telefone"),
-  nomeOrientador: yup.string().required("Informe o nome do seu orientador"),
-  emailOrientador: yup.string().email("Email inválido").required("Informe o email do seu orientador"),
-  telefoneOrientador: yup.string().required("Informe o telefone"),
-  departamento: yup.string().required("Informe o departamento"),
-  naturezaProjeto: yup.string().required("Informe a natureza do projeto"),
-  descricao: yup.string().required("Informe a descrição")
-});
-
-// function addInTable(values: {
-//   firstName: string;
-//   surname: string;
-//   email: string;
-//   password: string;
-//   birthdayDay: number;
-//   birthdayMonth: number;
-//   birthdayYear: number;
-// }) {
-//   try {
-
-//   } catch (error) {
-//     console.error("error", error);
-//   }
-// }
-
-async function handleClickForm(values: {
-  nomeAluno: string;
-  emailAluno: string;
-  telefoneAluno: string;
-  nomeOrientador: string;
-  emailOrientador: string;
-  telefoneOrientador: string;
-  departamento: string;
-  naturezaProjeto: string;
-  descricao: string;
-}) {
-  try {
-
-  } catch (error) {
-    console.error("error", error);
+export function FormDrx() {
+    interface RowData {
+    amostra: number;
+    identificacao: string;
+    modo: string;
+    faixa: string;
+    velocidade: string;
+    step: string;
+    tempo: string;
   }
-}
+  
+  const [rows, setRows] = useState<RowData[]>([]);
+  
+  function createData(amostra: number, identificacao: string, modo: string, faixa: string, velocidade: string, step: string, tempo: string) {
+    // return { amostra, identificacao, modo, faixa, velocidade, step, tempo };
+    const newData = { amostra, identificacao, modo, faixa, velocidade, step, tempo };
+    setRows([...rows, newData]);
+  }
+  
+  
+  const validationForm = yup.object().shape({
+    nomeAluno: yup.string().required("Informe seu nome"),
+    emailAluno: yup.string().email("Email inválido").required("Informe seu email"),
+    telefoneAluno: yup.string().required("Informe seu telefone"),
+    nomeOrientador: yup.string().required("Informe o nome do seu orientador"),
+    emailOrientador: yup.string().email("Email inválido").required("Informe o email do seu orientador"),
+    telefoneOrientador: yup.string().required("Informe o telefone"),
+    departamento: yup.string().required("Informe o departamento"),
+    naturezaProjeto: yup.string().required("Informe a natureza do projeto"),
+    descricao: yup.string().required("Informe a descrição")
+  });
+  
+  function addInTable(values: {
+    amostra: number;
+    identificacao: string;
+    modo: string;
+    faixa: string;
+    velocidade: string;
+    step: string;
+    tempo: string;
+  }) {
+    createData(values.amostra, values.identificacao, values.modo, values.faixa, values.velocidade, values.step, values.tempo)
+  }
+  
+  async function handleClickForm(values: {
+    nomeAluno: string;
+    emailAluno: string;
+    telefoneAluno: string;
+    nomeOrientador: string;
+    emailOrientador: string;
+    telefoneOrientador: string;
+    departamento: string;
+    naturezaProjeto: string;
+    descricao: string;
+  }) {
+    try {
+      console.log(rows)
+    } catch (error) {
+      console.error("error", error);
+    }
+  }
 
-export const FormDrx: React.FC = () => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>DRX</h1>
-    <div>
-      <Formik
-        initialValues={{
-          nomeAluno: "",
-          emailAluno: "",
-          telefoneAluno: "",
-          nomeOrientador: "",
-          emailOrientador: "",
-          telefoneOrientador: "",
-          departamento: "",
-          naturezaProjeto: "",
-          descricao: "",
-        }}
-        onSubmit={handleClickForm}
-        validationSchema={validationForm}
-      >
-        <Form className={styles.inputs_container}>
-          <div className={styles.inputs_box}>
-            <FormHeader />
-            <h3 className={styles.sub_title}>METODOLOGIA DE ANÁLISE</h3>
-            <div className={styles.row_box}>
-              <div className={styles.field_box}>
-                <p>Nº Amostra</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="coluna"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="coluna"
-                    placeholder=''
-                    className={styles.input_form}
-                  />
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>DRX</h1>
+      <div>
+        <Formik
+          initialValues={{
+            nomeAluno: "",
+            emailAluno: "",
+            telefoneAluno: "",
+            nomeOrientador: "",
+            emailOrientador: "",
+            telefoneOrientador: "",
+            departamento: "",
+            naturezaProjeto: "",
+            descricao: "",
+            amostra: 0,
+            identificacao: "",
+            modo: "",
+            faixa: "",
+            velocidade: "",
+            step: "",
+            tempo: ""
+          }}
+          onSubmit={handleClickForm}
+          validationSchema={validationForm}
+        >
+          {({ values }) => (
+            <Form className={styles.inputs_container}>
+              <div className={styles.inputs_box}>
+                <FormHeader />
+                <h3 className={styles.sub_title}>METODOLOGIA DE ANÁLISE</h3>
+                <div className={styles.row_box}>
+                  <div className={styles.field_box}>
+                    <p>Nº Amostra</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="amostra"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="amostra"
+                        placeholder=''
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Identificação</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="identificacao"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="identificacao"
+                        placeholder=''
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Modo de análise</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="modo"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="modo"
+                        placeholder='Step/Contínuo'
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Faixa de Varredura</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="faixa"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="faixa"
+                        placeholder='10º -70º'
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Velocidade de Varredura</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="velocidade"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="velocidade"
+                        placeholder='º/min'
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Step</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="step"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="step"
+                        placeholder='0,02 ou 0,05'
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className={styles.field_box}>
+                    <p>Tempo do passo</p>
+                    <div className={styles.input_box}>
+                      <ErrorMessage
+                        component={CustomErrorMessage}
+                        name="tempo"
+                        className={styles.form_error}
+                      />
+                      <Field
+                        name="tempo"
+                        placeholder='Somente step'
+                        className={styles.input_form}
+                      />
+                    </div>
+                  </div>
+                  <div className='button_box'>
+                    <button type='button' onClick={() => addInTable(values)}>
+                      <Add style={{ color: '#3f51b5' }} />
+                    </button>
+                  </div>
                 </div>
+                <TableContainer component={Paper}>
+                  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell align="left">Nº Amostra</TableCell>
+                        <TableCell align="right">Identificação</TableCell>
+                        <TableCell align="right">Modo de análise</TableCell>
+                        <TableCell align="right">Faixa de Varredura</TableCell>
+                        <TableCell align="right">Velocidade de Varredura</TableCell>
+                        <TableCell align="right">Step</TableCell>
+                        <TableCell align="right">Tempo do passo</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.amostra}
+                          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                          <TableCell component="th" scope="row">{row.amostra}</TableCell>
+                          <TableCell align="right">{row.identificacao}</TableCell>
+                          <TableCell align="right">{row.modo}</TableCell>
+                          <TableCell align="right">{row.faixa}</TableCell>
+                          <TableCell align="right">{row.velocidade}</TableCell>
+                          <TableCell align="right">{row.step}</TableCell>
+                          <TableCell align="right">{row.tempo}</TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               </div>
-              <div className={styles.field_box}>
-                <p>Identificação</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder=''
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className={styles.field_box}>
-                <p>Modo de análise</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder='Step/Contínuo'
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className={styles.field_box}>
-                <p>Faixa de Varredura</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder='10º -70º'
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className={styles.field_box}>
-                <p>Velocidade de Varredura</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder='º/min'
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className={styles.field_box}>
-                <p>Step</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder='0,02 ou 0,05'
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className={styles.field_box}>
-                <p>Tempo do passo</p>
-                <div className={styles.input_box}>
-                  <ErrorMessage
-                    component={CustomErrorMessage}
-                    name="gas"
-                    className={styles.form_error}
-                  />
-                  <Field
-                    name="gas"
-                    placeholder='Somente step'
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-              <div className='button_box'>
-                <button>
-					        <Add style={{ color: '#3f51b5' }} />
-                </button>
-              </div>
-            </div>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Dessert (100g serving)</TableCell>
-                    <TableCell align="right">Calories</TableCell>
-                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow
-                      key={row.name}
-                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                      <TableCell component="th" scope="row">{row.name}</TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </div>
-          <FormFooter /> 
-        </Form>
-      </Formik>
+              <FormFooter /> 
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
-  </div>
-)
+  )
+}
--- src/components/form-header/index.tsx
+++ src/components/form-header/index.tsx
@@ -5,154 +5,152 @@ import { CustomErrorMessage } from '@/components'
 import styles from "./styles.module.scss";
 
 export const FormHeader: React.FC = () => (
-	<Form className={styles.inputs_container}>
-		<div className={styles.inputs_box}>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Nome do Aluno</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="nomeAluno"
-						/>
-						<Field
-							name="nomeAluno"
-							placeholder=""
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Email do Aluno</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="emailAluno"
-						/>
-						<Field
-							name="emailAluno"
-							placeholder=""
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-				<div className={styles.field_box}>
-					<p>Telefone do Aluno</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="telefoneAluno"
-						/>
-						<Field
-							name="telefoneAluno"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Nome do Orientador</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="nomeOrientador"
-							className={styles.form_error}
-						/>
-						<Field
-							name="nomeOrientador"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Email do Orientador</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="emailOrientador"
-							className={styles.form_error}
-						/>
-						<Field
-							name="emailOrientador"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-				<div className={styles.field_box}>
-					<p>Telefone do Orientador</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="telefoneOrientador"
-							className={styles.form_error}
-						/>
-						<Field
-							name="telefoneOrientador"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Natureza do Projeto</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="naturezaProjeto"
-							className={styles.form_error}
-						/>
-						<Field
-							name="naturezaProjeto"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-				<div className={styles.field_box}>
-					<p>Departamento</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="departamento"
-							className={styles.form_error}
-						/>
-						<Field
-							name="departamento"
-							placeholder=''
-							className={styles.input_form}
-						/>
-					</div>
-				</div>
-			</div>
-			<div className={styles.row_box}>
-				<div className={styles.field_box}>
-					<p>Descrição</p>
-					<div className={styles.input_box}>
-						<ErrorMessage
-							component={CustomErrorMessage}
-							name="descricao"
-							className={styles.form_error}
-						/>
-						<Field
-							component="textarea"
-							name="descricao"
-							type="textarea"
-							placeholder='DESCREVER A METODOLOGIA DE PREPARO DAS AMOSTRAS A SEREM ANALISADAS:'
-							className={styles.input_form_text_area}
-						/>
-					</div>
+	<div className={styles.inputs_box}>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Nome do Aluno</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="nomeAluno"
+					/>
+					<Field
+						name="nomeAluno"
+						placeholder=""
+						className={styles.input_form}
+					/>
 				</div>
 			</div>
 		</div>
-	</Form>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Email do Aluno</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="emailAluno"
+					/>
+					<Field
+						name="emailAluno"
+						placeholder=""
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+			<div className={styles.field_box}>
+				<p>Telefone do Aluno</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="telefoneAluno"
+					/>
+					<Field
+						name="telefoneAluno"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+		</div>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Nome do Orientador</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="nomeOrientador"
+						className={styles.form_error}
+					/>
+					<Field
+						name="nomeOrientador"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+		</div>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Email do Orientador</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="emailOrientador"
+						className={styles.form_error}
+					/>
+					<Field
+						name="emailOrientador"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+			<div className={styles.field_box}>
+				<p>Telefone do Orientador</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="telefoneOrientador"
+						className={styles.form_error}
+					/>
+					<Field
+						name="telefoneOrientador"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+		</div>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Natureza do Projeto</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="naturezaProjeto"
+						className={styles.form_error}
+					/>
+					<Field
+						name="naturezaProjeto"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+			<div className={styles.field_box}>
+				<p>Departamento</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="departamento"
+						className={styles.form_error}
+					/>
+					<Field
+						name="departamento"
+						placeholder=''
+						className={styles.input_form}
+					/>
+				</div>
+			</div>
+		</div>
+		<div className={styles.row_box}>
+			<div className={styles.field_box}>
+				<p>Descrição</p>
+				<div className={styles.input_box}>
+					<ErrorMessage
+						component={CustomErrorMessage}
+						name="descricao"
+						className={styles.form_error}
+					/>
+					<Field
+						component="textarea"
+						name="descricao"
+						type="textarea"
+						placeholder='DESCREVER A METODOLOGIA DE PREPARO DAS AMOSTRAS A SEREM ANALISADAS:'
+						className={styles.input_form_text_area}
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
 )
