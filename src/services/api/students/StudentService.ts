4a6f84d6311815f474c23f2a5206bb6a48e73461
marco-rozo <marcorozo99@gmail.com>
2023-06-12 22:22:14 -0300

    fix: changed route

--- src/services/api/students/StudentService.ts
+++ src/services/api/students/StudentService.ts
@@ -1,7 +1,7 @@
 import { api } from '@/libs'
 
 // substituir pra rota que retoprne os estudantes daquele professor, ou todos os usuarios do tipo estudantes
-const findAll = () => api.get('/users')
+const findAll = () => api.get('/users/role/STUDENT')
 
 const findById = (id: number) => api.get(`/users/${id}`)
 
