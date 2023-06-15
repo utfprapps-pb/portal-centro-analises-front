3d2999afd9d4d376c9d66a9c64523c52777229ef
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2023-06-14 21:00:54 -0300

    added component datatable to admin panel

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import { Header, Menu, AdminPanel } from '@/components'
+import DataTable from '@/components/data-table'
 
 export const AdminPage: React.FC = () => (
   <div className={styles.container}>
@@ -8,6 +9,7 @@ export const AdminPage: React.FC = () => (
     <div className={styles.middle}>
       <Header />
       <AdminPanel />
+      <DataTable />
     </div>
   </div>
 )
