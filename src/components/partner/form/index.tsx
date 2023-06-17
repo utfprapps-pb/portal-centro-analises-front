e369237ae1291fb5e879fd13ed4aac2dd4759c28
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-06-17 19:21:30 -0300

    ajuste: form

--- src/components/partner/form/index.tsx
+++ src/components/partner/form/index.tsx
@@ -1,29 +1,30 @@
 import { ChangeEvent, useEffect, useState } from 'react'
 
-import { Button } from '@material-ui/core'
+import { Button, TextField } from '@material-ui/core'
 import { useNavigate, useParams } from 'react-router-dom'
 import * as yup from 'yup'
 
 import styles from './styles.module.scss'
 import PartnerService from '../../../services/api/partner/service'
+import { Partner } from '../model/partner'
+import { Field, Form, Formik } from 'formik'
 
-export function Partner() {
-  const [form, setForm] = useState({
-    id: undefined,
+export function PartnerForm() {
+  const { id } = useParams()
+  const [partner, setPartner] = useState<Partner>({
     name: ''
   })
   const [errors, setErrors] = useState({ id: null, name: '' })
   const [pendingApiCall, setPendingApiCall] = useState(false)
   const [apiError, setApiError] = useState('')
   const navigate = useNavigate()
-  const { id } = useParams()
 
   useEffect(() => {
     if (id) {
       PartnerService.findOne(parseInt(id))
         .then((response) => {
           if (response.data) {
-            setForm({
+            setPartner({
               id: response.data.id,
               name: response.data.name
             })
@@ -38,29 +39,18 @@ export function Partner() {
     }
   }, [id])
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.target
-    setForm((previousForm) => {
-      return {
-        ...previousForm,
-        [name]: value
-      }
-    })
-    setErrors((previousErrors) => {
-      return {
-        ...previousErrors,
-        [name]: undefined
-      }
-    })
-  }
+  const validationSchema = yup.object().shape({
+    name: yup.string().min(4,"Deve informar no mínimo 4 caracteres").required("Nome é obrigatório"),
+  });
 
-  const onSubmit = () => {
-    const partner: any = {
-      id: form.id,
-      name: form.name
+  const onSubmit = (values: Partner) => {
+    const data: Partner = {
+       ...values,
+      id: partner.id,
+      name: values.name
     }
     setPendingApiCall(true)
-    PartnerService.save(partner)
+    PartnerService.save(data)
       .then((response) => {
         setPendingApiCall(false)
         navigate('/partner')
@@ -79,29 +69,33 @@ export function Partner() {
     <>
       <div className={styles.container}>
         <h1 className={styles.title}>Instituição Parceira</h1>
-        <form className={styles.inputs_container}>
-          <div className={styles.inputs_box}>
-            <div className={styles.row_box}>
-              <div className={styles.field_box}>
-                <p>Nome da Instituição</p>
-                <div className={styles.input_box}>
-                  <input
-                    name="name"
-                    placeholder=""
-                    value={form.name}
-                    onChange={onChange}
-                    className={styles.input_form}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+         <Formik
+            initialValues={partner}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+            enableReinitialize={true}
+          >
+            {({ errors, touched }) => (
+              <Form className={styles.form}>
+                <Field
+                  as={TextField}
+                  className={styles.textField}
+                  label="Nome"
+                  name="name"
+                  error={touched.name && !!errors.name}
+                  helperText={touched.name && errors.name}
+                  fullWidth
+                  required
+                  variant="outlined"
+                />
           <div className={styles.button_box}>
-            <Button variant="contained" color="primary" onClick={onSubmit}>
+            <Button variant="contained" color="primary" type="submit">
               Salvar
             </Button>
           </div>
-        </form>
+        </Form>
+            )}
+          </Formik>
       </div>
     </>
   )
--- src/pages/partner/form/index.tsx
+++ src/pages/partner/form/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import styles from './styles.module.scss'
-import { Header, Menu, Partner, PartnerList } from '@/components'
+import { Header, Menu, PartnerForm, PartnerList } from '@/components'
 
 export const PartnerPage: React.FC = () => (
   <div className={styles.container}>
     <Menu />
     <div className={styles.middle}>
       <Header />
-      <Partner />
+      <PartnerForm />
     </div>
   </div>
 )
