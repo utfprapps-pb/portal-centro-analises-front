76f117670314039d1080fdc966b3bef2b53cc262
GiovaneWerlang <giovanewerlang@alunos.utfpr.edu.br>
2023-08-20 15:46:25 -0300

    admin form dialog

--- src/components/admin-panel/index.tsx
+++ src/components/admin-panel/index.tsx
@@ -7,10 +7,21 @@ import { CustomButton } from '../custom-button';
 import Dropdown from '../dropdown';
 import { api } from '@/libs/axiosBase';
 import { EditUser } from '@/commons/type';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
 export function AdminPanel() {
 
   const [activePage, setActivePage] = useState(0);
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const [user, setUser] = useState<EditUser | undefined>();
 
@@ -24,6 +35,10 @@ export function AdminPanel() {
   });
 
   useEffect(() => {
+    loadData()
+  }, [activePage]);
+
+  function loadData() {
     api.get("/users")
       .then(response => {
         const data = response.data;
@@ -32,7 +47,7 @@ export function AdminPanel() {
           content: data
         }));
       })
-  }, [activePage]);
+  }
 
   const changePage = (index: number) => {
     setActivePage(index);
@@ -40,12 +55,15 @@ export function AdminPanel() {
 
   function getUser(selected: EditUser): void | undefined {
     setUser(selected);
+    handleClickOpen();
   }
 
   function updateSelectedUser(selected: EditUser) {
     if (selected != null && selected.id != null) {
       api.post(`admin/edit/role/${selected.id}`, selected.role).then((response) => {
-        window.location.reload();
+        handleClose();
+        loadData();
+        //window.location.reload();
       });
     }
   }
@@ -54,7 +72,9 @@ export function AdminPanel() {
     if (selected != null && selected.id != null) {
       api.delete('users/' + selected.id)
         .then((response) => {
-          window.location.reload();
+          handleClose();
+          loadData();
+          //window.location.reload();
         });
 
     }
@@ -93,91 +113,113 @@ export function AdminPanel() {
       ) : (
         <><div className={styles.inputs_box}>
           <div className={styles.container}>
+            <Dialog open={open} onClose={handleClose}  style={{ overflowY: 'visible' }}>
+              <DialogTitle>Usuário</DialogTitle>
+              <DialogContent style={{ overflowY: 'visible' }}>
+                <Formik
+                  initialValues={{
+                    name: "",
+                    cargo: "",
+                    orientador: "",
+                    email: ""
+                  }}
+                  onSubmit={handleClickForm}
+                  validationSchema={validationForm}
+                >
+                  <Form className={styles.inputs_container}>
+                    <div className={styles.inputs_box}>
+                      <div className={styles.row_box}>
+                        <div className={styles.field_box}>
+                          <p>Nome</p>
+                          <div className={styles.input_box}>
+                            <ErrorMessage
+                              component={CustomErrorMessage}
+                              name="nome"
+                              className={styles.form_error} />
+                            <Field
+                              name="nome"
+                              disabled
+                              value={user?.name ?? ''}
+                              placeholder=''
+                              className={styles.input_form} />
+                          </div>
+                        </div>
+                        <div className={styles.field_box}>
+                          <p>Email</p>
+                          <div className={styles.input_box}>
+                            <ErrorMessage
+                              component={CustomErrorMessage}
+                              name="email"
+                              className={styles.form_error} />
+                            <Field
+                              name="email"
+                              disabled
+                              value={user?.email ?? ''}
+                              placeholder=''
+                              className={styles.input_form} />
+                          </div>
+                        </div>
+                      </div>
+                      <div className={styles.row_box}>
+                        <div className={styles.field_box}>
+                          <div className={styles.field_box}>
+                            <p>Cargo</p>
+                            <Dropdown value={user?.role || ''} onChange={handleRoleChange} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Form>
+                </Formik>
+              </DialogContent>
+              <DialogActions>
+                <div className={styles.button_box}>
+                  <CustomButton
+                    onClick={() => handleClose()}
+                    text="Cancelar"
+                    padding="1rem"
+                    textColor="white"
+                    backgroundColor="#676767"
+                    textColorHover="white"
+                    backgroundColorHover="#9f9f9f"
+                    letterSpacing="4px"
+                    fontSize="16px"
+                    fontWeight="400"
+                    type="submit" />
+                </div>
+                <div className={styles.button_box}>
+                  <CustomButton
+                    onClick={() => removeUserSelectedUser(user!)}
+                    text="REMOVER"
+                    padding="1rem"
+                    textColor="white"
+                    backgroundColor="#cc0000"
+                    textColorHover="white"
+                    backgroundColorHover="#ff4444"
+                    letterSpacing="4px"
+                    fontSize="16px"
+                    fontWeight="400"
+                    type="submit" />
+                </div>
+                <div className={styles.button_box}>
+                  <CustomButton
+                    onClick={() => updateSelectedUser(user!)}
+                    text="ATUALIZAR"
+                    padding="1rem"
+                    textColor="white"
+                    backgroundColor="#006dac"
+                    textColorHover="white"
+                    backgroundColorHover="#00bbff"
+                    letterSpacing="4px"
+                    fontSize="16px"
+                    fontWeight="400"
+                    type="submit" />
+                </div>
+              </DialogActions>
+            </Dialog>
             <h1 className={styles.title}>PAINEL DO ADMINISTRADOR</h1>
             <div>
-              <Formik
-                initialValues={{
-                  name: "",
-                  cargo: "",
-                  orientador: "",
-                  email: ""
-                }}
-                onSubmit={handleClickForm}
-                validationSchema={validationForm}
-              >
-                <Form className={styles.inputs_container}>
-                  <div className={styles.inputs_box}>
-                    <div className={styles.row_box}>
-                      <div className={styles.field_box}>
-                        <p>Nome</p>
-                        <div className={styles.input_box}>
-                          <ErrorMessage
-                            component={CustomErrorMessage}
-                            name="nome"
-                            className={styles.form_error} />
-                          <Field
-                            name="nome"
-                            disabled
-                            value={user?.name ?? ''}
-                            placeholder=''
-                            className={styles.input_form} />
-                        </div>
-                      </div>
-                      <div className={styles.field_box}>
-                        <p>Email</p>
-                        <div className={styles.input_box}>
-                          <ErrorMessage
-                            component={CustomErrorMessage}
-                            name="email"
-                            className={styles.form_error} />
-                          <Field
-                            name="email"
-                            disabled
-                            value={user?.email ?? ''}
-                            placeholder=''
-                            className={styles.input_form} />
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles.row_box}>
-                      <div className={styles.field_box}>
-                        <div className={styles.field_box}>
-                          <p>Cargo</p>
-                          <Dropdown value={user?.role || ''} onChange={handleRoleChange} />
-                        </div>
-                      </div>
-                    </div>
-                    <div className={styles.button_box}>
-                      <CustomButton
-                        onClick={() => updateSelectedUser(user!)}
-                        text="ATUALIZAR"
-                        padding="1rem"
-                        textColor="white"
-                        backgroundColor="#006dac"
-                        textColorHover="white"
-                        backgroundColorHover="#00bbff"
-                        letterSpacing="4px"
-                        fontSize="16px"
-                        fontWeight="400"
-                        type="submit" />
-                    </div>
-                    <div className={styles.button_box}>
-                      <CustomButton
-                        onClick={() => removeUserSelectedUser(user!)}
-                        text="REMOVER"
-                        padding="1rem"
-                        textColor="white"
-                        backgroundColor="#cc0000"
-                        textColorHover="white"
-                        backgroundColorHover="#ff4444"
-                        letterSpacing="4px"
-                        fontSize="16px"
-                        fontWeight="400"
-                        type="submit" />
-                    </div>
-                  </div>
-                </Form>
-              </Formik>
+
             </div>
           </div>
         </div>
