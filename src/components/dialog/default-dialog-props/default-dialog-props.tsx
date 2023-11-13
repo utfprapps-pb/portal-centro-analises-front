1b4b6ad1334aac4e3d96a7a4f5568ee0091c2000
Erick Elias Borges <50887364+erickeliasborges@users.noreply.github.com>
2023-11-13 18:47:52 -0300

    Adicionado alerta de remover registros nas telas

--- src/components/dialog/default-dialog-props/default-dialog-props.tsx
+++ src/components/dialog/default-dialog-props/default-dialog-props.tsx
@@ -4,3 +4,8 @@ export const RemoveQuestionDefaultProps: DialogProps = {
   title: 'Deseja mesmo excluir o registro?',
   content: 'Essa ação não poderá ser revertida.'
 }
+
+export const InactivateQuestionDefaultProps: DialogProps = {
+  title: 'Deseja mesmo inativar o registro?',
+  content: 'Essa ação poderá ser revertida ao ativar o registro novamente.',
+}
